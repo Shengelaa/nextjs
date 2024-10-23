278b206362b17808b5fb9e1b94060216c4b9334f
OrionGift3 <168460082+Shengelaa@users.noreply.github.com>
2024-10-23 19:42:13 +0400

    dsad

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <a href='/about-us'>hello</a>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/app/posts/two/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page2 = () => {
+  return <div>2</div>;
+};
+
+export default page2;
